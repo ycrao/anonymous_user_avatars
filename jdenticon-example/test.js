e57fb24ad484ec9d3ed15ecf5ecf15fe5862f06e
raoyc <raoyc@foxmail.com>
2020-06-01 17:56:46 +0800

    update code

--- jdenticon-example/test.js
+++ jdenticon-example/test.js
@@ -1,7 +1,10 @@
-var jdenticon = require("jdenticon"),
+for (i = 0; i < 100; i ++) {
+    var jdenticon = require("jdenticon"),
     fs = require("fs"),
     size = 200,
-    value = "anonymous_user",
+    value = "anonymous_user_" + i,
     png = jdenticon.toPng(value, size);
     
-fs.writeFileSync("./anonymous_user_avatar.png", png);
+    fs.writeFileSync("./anonymous_avatars/" + i + ".png", png);
+}
+
